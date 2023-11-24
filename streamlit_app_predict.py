565e7fb1675b590f028861baadaebdf285c04c39
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-11-24 15:55:52 +0530

    detection-tyre

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -76,7 +76,7 @@ def main():
     
 
     rf = Roboflow(api_key="0Uglhm9vMkjvOzEnA7t2")
-    project = rf.workspace().project("tyre-classification-icjz7")
+    project = rf.workspace().project("detection-tyre")
     model = project.version(1).model
      
     image, svd_img = load_image()
